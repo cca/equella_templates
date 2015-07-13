87cc7f89302c7480179875e7e03bc9cab30106cd
phette23 <phette23@gmail.com>
2015-07-13 12:42:51 -0700

    portlets - student work gallery now fades in sequentially

--- Portlets/student-work-home.js
+++ Portlets/student-work-home.js
@@ -1,17 +1,9 @@
 // put under "Client-side on-ready script" in the portal admin
 
-var alignImages = function ($el) {
-    // make studentWork a bit shorter
-    var height = $el.attr('id') == 'studentWork' ? 90 : 130
-    $el.justifiedGallery({
-        rowHeight: height
-    });
-}
-
 var handleTabClick = function (event) {
-    var $el = $(this)
+    var $targetTab = $(this)
     // cache jQuery lookups
-    , $target = $($el.data('target'))
+    , $target = $($targetTab.data('target'))
     , $activeTab = $('.tab.active')
     , $active = $($activeTab.data('target'))
 
@@ -23,11 +15,40 @@ var handleTabClick = function (event) {
         $active.hide()
         $activeTab.removeClass('active')
         // add active class to clicked tab, show new gallery
-        $el.addClass('active')
-        $target.show()
-        alignImages($target)
+        $targetTab.addClass('active')
+        $target.show() // is this necessary? does JG do .show() automatically?
+        showImages($target)
     }
 }
 
-alignImages($('#studentWork'))
+var timers = []
+
+var showImages = function ($wrapper) {
+    var height = $wrapper.attr('id') == 'studentWork' ? 90 : 130
+    var $images = $wrapper.find('img')
+
+    // clear existing timers
+    $.each(timers, function(index, timer) {
+        clearTimeout(timer)
+    })
+    // hide all images
+    $images.hide();
+    // justified gallery
+    $wrapper.justifiedGallery({
+        rowHeight: height
+    })
+    // when layout's complete, fade in each image
+    // each one has a 200s longer interval than the last
+    .on('jg.complete', function() {
+        $images.each(function(index, img) {
+            timers.push(
+                setTimeout(function() {
+                    $(img).fadeIn();
+                }, 200 * index)
+            );
+        })
+    })
+}
+
+showImages($('#studentWork'))
 $('.tab').click(handleTabClick)
