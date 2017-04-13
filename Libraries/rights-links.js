32e8955265fd7dc46673f7376e309a07aec1654e
phette23 <phette23@gmail.com>
2017-04-13 12:05:42 -0700

    Libraries - slight change to accessCondition value

--- Libraries/rights-links.js
+++ Libraries/rights-links.js
@@ -1,6 +1,6 @@
 // linkify any plain text URLs in the mods/accessCondition section
 // dd.rights is the wrapper HTML tag
 var $el = $('.rights')
-var re = /(https:\/\/creativecommons\.org\/licenses\/by-nc\/4\.0\/)\./i
-var html = $el.html().replace(re, "<a rel='license' href='$1'>$1</a>.")
+var re = /(https:\/\/creativecommons\.org\/licenses\/by-nc\/4\.0\/) \./i
+var html = $el.html().replace(re, '<a rel="license" href="$1">$1</a>.')
 $el.html(html)
